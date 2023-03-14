dbfe5a350c239ae037e6459b21f2dc4baac6df9d
Russell Martin <rmartin16@gmail.com>
2023-03-14 13:28:51 -0400

    testing

--- src/briefcase/commands/create.py
+++ src/briefcase/commands/create.py
@@ -474,7 +474,6 @@ class CreateCommand(BaseCommand):
                             "pip",
                             "install",
                             "--upgrade",
-                            "--no-build-isolation",
                             # "--no-user",
                             f"--target={app_packages_path}",
                         ]
